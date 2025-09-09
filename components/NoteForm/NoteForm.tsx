a5ca8d3079315db4f40f26b0714c5f1240c8122e
Yaroslav <yaroslavlit@gmail.com>
2025-09-09 03:14:18 +0200

    fix routes v1.3

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -5,7 +5,7 @@ import { useMutation } from "@tanstack/react-query";
 import { createNote } from "@/lib/api";
 import LoadingButton from "../LoadingButton/LoadingButton";
 import { useRouter } from "next/navigation";
-import { useNoteStore } from "@/stores/noteStore";
+import { useNoteStore } from "@/lib/store/noteStore";
 import { Note } from "@/types/note";
 
 export default function NoteForm() {
